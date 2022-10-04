209b3c3f39242a6c195230d2136dd1a641f9625e
Shrike <apaintree@yandex.ru>
2022-10-04 16:11:40 +0300

    added time sleep

--- test_items.py
+++ test_items.py
@@ -1,8 +1,10 @@
+import time
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_language_interface(browser):
     browser.get(link)
+    time.sleep(10)
     button_selector = browser.find_elements(By.CLASS_NAME, "btn.btn-lg.btn-primary.btn-add-to-basket")
     assert button_selector, "Button selector not found"
